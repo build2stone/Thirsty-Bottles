d9619b1c22eb050d20f62e3da071153241589d6f
build2stone <36826079+build2stone@users.noreply.github.com>
2018-03-04 21:20:42 +0100

    This better work

--- src/main/java/net/darkhax/tb/ThirstyBottles.java
+++ src/main/java/net/darkhax/tb/ThirstyBottles.java
@@ -1,5 +1,9 @@
 package net.darkhax.tb;
 
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -8,19 +12,12 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
-import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemGlassBottle;
-import net.minecraft.item.ItemStack;
-import net.minecraft.stats.StatList;
-import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent.RightClickBlock;
 import net.minecraftforge.fluids.IFluidBlock;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -28,32 +25,31 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class ThirstyBottles {
 
     private static final Logger LOG = LogManager.getLogger("Thirsty Bottles");
-    
-	@EventHandler
-	public void preInit(FMLPreInitializationEvent event) {
 
-		MinecraftForge.EVENT_BUS.register(this);
-	}
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
 
-	@SubscribeEvent
-	public void onItemUsed(RightClickBlock event) {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
 
-		if (event.getWorld().isRemote)
-			return;
-		
+    @SubscribeEvent
+    public void onItemUsed(PlayerInteractEvent.RightClickItem event) {
+
+        if (event.getWorld().isRemote)
+            return;
+
         if (event.getItemStack() != null && event.getItemStack().getItem() instanceof ItemGlassBottle) {
 
             World world = event.getWorld();
             EntityPlayer player = event.getEntityPlayer();
 
             Vec3d look = player.getLookVec();
-            float f = 5.0F;
+            float f = 5.0F; // distance factor
             Vec3d start = new Vec3d(player.posX, player.posY + player.getEyeHeight(), player.posZ);
             Vec3d end = new Vec3d(player.posX + look.x * f, player.posY + player.getEyeHeight() + look.y * f, player.posZ + look.z * f);
-            RayTraceResult raytraceresult = world.rayTraceBlocks(start, end, true, false, true);
+            RayTraceResult raytraceresult= world.rayTraceBlocks(start, end, true, false, true);
 
             if (raytraceresult != null) {
-
                 BlockPos pos = raytraceresult.getBlockPos();
                 IBlockState state = event.getWorld().getBlockState(pos);
 
@@ -62,10 +58,5 @@ public class ThirstyBottles {
                 }
             }
         }
-    
-    @EventHandler
-    public void onFingerprintViolation (FMLFingerprintViolationEvent event) {
-
-        LOG.error("Invalid fingerprint detected! The file " + event.getSource().getName() + " may have been tampered with. This version will NOT be supported by the author!");
     }
 }
